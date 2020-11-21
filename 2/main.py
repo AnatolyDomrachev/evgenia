076604c545c43ab96d124c367537a20f5232ca6d
Ваше Имя <you@example.com>
2020-11-21 22:19:36 +0300

    1

--- 2/main.py
+++ 2/main.py
@@ -1,6 +1,13 @@
+def view_contacts():
+def add_new_contact():
+def exit_the_application():
+
 print(Choose an option:)
 1 - view contacts
 2 - add new contact
 3 - exit the application
 ch1 = int(input(Your choice: )) 
 
+if ch1 = 1 : view_contacts()
+if ch1 = 2 : add_new_contact()
+if ch1 = 3 : exit_the_application()
